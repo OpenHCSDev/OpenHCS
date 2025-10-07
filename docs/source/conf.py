15964f2527c8813aa8e63b384d95788046ba345d
jathav <jathav.puvirajan@mail.mcgill.ca>
2025-10-07 14:57:07 -0400

    intro stitching guide

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,6 +76,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx_rtd_theme',
     'sphinx_toolbox.collapse',
+    'sphinx_design',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
